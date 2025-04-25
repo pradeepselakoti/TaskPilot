e3be2d5cd0c4f1319d171b06578c3ae28670baa6
karthik-kiran-29 <karthikkiran029@gmail.com>
2025-04-25 14:15:44 +0530

    add task, task assignment, and task update models

---
+++ backend/models/TaskUpdate.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+
+const taskUpdateSchema = new mongoose.Schema({
+  task_id: { type: Schema.Types.ObjectId, ref: 'Task', required: true },
+  user_id: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  status: { type: String , enum: ['completed', 'in-progress', 'pending'], required: true},
+  updated_at: { type: Date, default: Date.now }
+}, { timestamps: true });
+
+const TaskUpdate = mongoose.model('TaskUpdate', taskUpdateSchema);
+
+export default TaskUpdate;
