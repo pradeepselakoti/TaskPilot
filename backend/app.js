aee14e2967242f05766058d56b59485d71e55e0a
Kartikey Joshi <joshikartikey32@gmail.com>
2025-04-29 21:56:10 +0530

    added readme

--- backend/app.js
+++ backend/app.js
@@ -2,28 +2,31 @@ import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
+import dotenv from 'dotenv';
 import userRoutes from './routes/userRoutes.js';
 import authRoutes from './routes/authRoutes.js';
 import authMiddleware from './middlewares/Auth.js';
+
+dotenv.config();
+
 const app = express();
+
 app.use(express.json());
 app.use(cors({
-    origin:[process.env.FrontendURI],
-    credentials:true
+    origin: [process.env.FrontendURI],
+    credentials: true
 }));
 app.use(cookieParser());
 app.use(morgan('dev'));
-app.use(express.json());
 
-app.use('/ping',(req,res)=>{
+app.use('/ping', (req, res) => {
     res.send('pong');
-})
+});
 
-//userRoute
-app.use('/api/v1/auth',authRoutes);
-app.use('/api/v1/user', authMiddleware,userRoutes);
+app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/user', authMiddleware, userRoutes);
 
-app.all('*', (req,res)=>{
+app.all('*', (req, res) => {
     res.status(404).send('OPPS 404 ,PAGE NOT FOUND');
 });
 
