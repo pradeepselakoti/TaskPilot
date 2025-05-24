93f7c3e956c161199b282c35341e9148f8cdfa26
Kartikey Joshi <joshikartikey32@gmail.com>
2025-05-24 20:56:44 +0530

    Resolved merge conflict in TaskRoute.js by using admin and tl roles for assignment routes

--- backend/routes/TaskRoute.js
+++ backend/routes/TaskRoute.js
@@ -9,13 +9,10 @@ const router = express.Router();
 
 router.route('/:id').get(getTaskById).put(checkRole(["tl","admin"]),updateTask).delete(checkRole(['admin',"tl"]),deleteTask);
 
-<<<<<<< HEAD
-router.post('/assign',checkRole("tl"),assignToIntern)
+router.post('/assign',checkRole("tl", "admin"),assignToIntern)
 router.patch('/:id/assign/:assignment_id/verify',checkRole("tl"),verifyCompletion)
-=======
 router.post('/:id/assign',checkRole(["tl","admin"]),assignToIntern)
 router.patch('/:id/assign/:assignment_id/verify',checkRole(["tl","admin"]),verifyCompletion)
->>>>>>> 93950c7afccf1798819813a953f7c3be1f387830
 router.route('/:id/update').post(checkRole("intern"),createUpdate).get(listUpdates)
 
 
