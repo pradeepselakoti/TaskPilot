efdf092cbc78555e5d9300ec40334012f4f6117b
Kartikey Joshi <joshikartikey32@gmail.com>
2025-05-24 20:49:16 +0530

    Your commit message describing changes to routes

--- backend/routes/ProjectRoute.js
+++ backend/routes/ProjectRoute.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import checkRole from '../middlewares/role.js';
 import { createProject, deleteProject, getProjectById, getProjects, updateProject } from '../controllers/ProjectController.js';
+import { createProjectAssignment, deleteProjectAssignment, getProjectAssignmentById, getAllProjectAssignments, updateProjectAssignmentById } from '../controllers/ProjectAssignmentController.js';
 import { getTimeline } from '../controllers/ProjectTimelineController.js';
 import {createTask,  listTasks } from '../controllers/taskController.js';
 
@@ -9,8 +10,11 @@ const router = express.Router();
 
 router.route('/').post(checkRole('cos'),createProject).get(getProjects);
 router.route('/:id').get(getProjectById).put(checkRole("cos"),updateProject).delete(checkRole('admin'),deleteProject);
+
+router.route('/create-assignment').post(checkRole('cos'),createProjectAssignment).get(getAllProjectAssignments);
+router.route('/assignment/:id').get(checkRole("cos"),getProjectAssignmentById).put(updateProjectAssignmentById).delete(checkRole('admin'),deleteProjectAssignment);
+
 router.route('/:id/timeline').get(getTimeline);
 router.route('/:id/tasks').post(checkRole('tl'),createTask).get(listTasks);
-
 
 export default router;
--- backend/routes/TaskRoute.js
+++ backend/routes/TaskRoute.js
@@ -9,7 +9,7 @@ const router = express.Router();
 
 router.route('/:id').get(getTaskById).put(checkRole("tl"),updateTask).delete(checkRole(['admin',"tl"]),deleteTask);
 
-router.post('/:id/assign',checkRole("tl"),assignToIntern)
+router.post('/assign',checkRole("tl"),assignToIntern)
 router.patch('/:id/assign/:assignment_id/verify',checkRole("tl"),verifyCompletion)
 router.route('/:id/update').post(checkRole("intern"),createUpdate).get(listUpdates)
 
