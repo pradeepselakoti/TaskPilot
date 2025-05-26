e2b6e4a31a90a9932eda1f8ebad8f1900fe37c20
karthik-kiran-29 <karthikkiran029@gmail.com>
2025-05-27 01:28:31 +0530

    Refactor getTimeline function: replace ProjectTimeline retrieval with Task queries, enhance response structure to include member details.

--- backend/controllers/ProjectTimelineController.js
+++ backend/controllers/ProjectTimelineController.js
@@ -1,29 +1,31 @@
-import ProjectTimeline from '../models/ProjectTimeline.js';
+import Task from "../models/Task.js";
 
 export const getTimeline = async (req, res) => {
     try {
-        const { projectId } = req.params;
-
-        const timeline = await ProjectTimeline.findOne({ project_id: projectId })
-            .populate({
-                path: 'task_updates.task_id',
-                select: 'title description due_date'
-            })
-            .populate({
-                path: 'project_id',
-                select: 'name start_date end_date'
-            });
-
-        if (!timeline) {
-            return res.status(404).json({ 
-                success: false, 
-                message: 'Timeline not found for this project' 
+        const { id } = req.params;
+        if (!id) {
+            return res.status(400).json({
+                success: false,
+                message: 'Project ID is required'
             });
         }
 
+        const timelines = await Task.find({ project_id: id },{project_id: 1,start_date: 1, end_date: 1, status: 1,assigned_to: 1})
+        .populate('assigned_to', 'first_name last_name temp_role').lean();
+
+        timelines.map(task => {
+            task.member_id = task.assigned_to._id;
+            task.member_name = `${task.assigned_to.first_name} ${task.assigned_to.last_name}`;
+            task.member_role = task.assigned_to.temp_role;
+
+            delete task.assigned_to;
+            return task;
+
+        });
+            
         return res.status(200).json({
             success: true,
-            data: timeline
+            data: timelines
         });
     } catch (error) {
         return res.status(500).json({
